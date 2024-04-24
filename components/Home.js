d8f3797050ba668333533447ff9a146fb368cf40
NaeemDy <naim.hamidi@gmail.com>
2024-04-24 17:29:20 +0200

    second commit

--- components/Home.js
+++ components/Home.js
@@ -34,7 +34,7 @@ function Home() {
   );
   const [moviesDataFromAPI, setMoviesDataFromAPI] = useState([])
   useEffect(() => {
-    fetch('my-moviz-backend-peach.vercel.app')
+    fetch('https://my-moviz-frontend-kappa.vercel.app/')
       .then(response => response.json()) 
       .then(data => {
         setMoviesDataFromAPI(data.movies)
